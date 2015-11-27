b9a1a7d1a53643932f0070903e2e95c8a4465403
Leonardo Fernandez <leonfs@gmail.com>
2015-11-27 19:53:40 +0000

    - Added tests with functions pipelining

--- test/resources/test.sh
+++ test/resources/test.sh
@@ -6,6 +6,9 @@ echo "\n########### Begin test.sh ###################"
 echo $PWD
 echo "ASSERT: Static env: ${STATIC_ENV}"
 echo "ASSERT: Print query: ${URL}"
+echo "ASSERT: JsonQuery. First key from object. Value: ${NEW_TAG}"
+echo "ASSERT: JsonQuery. Piping Functions. Value: ${ENVIRONMENT}"
+echo "ASSERT: JsonQuery. First array. Value: ${FIRST_FROM_ARRAY}"
 echo "ASYNC_ASSERT: Creating file in secure path.."
 echo "${URL}" > "${REPOSITORY_NAME}"
 
--- test/runnable-test.js
+++ test/runnable-test.js
@@ -10,25 +10,8 @@ lugg.init();
 
 describe("Runnable", function() {
 	var runnable = require('../lib/runnable');
-	var existingScriptPath = 'test.sh';
-	var jsonQueries = {}
 
-
-
-	var staticEnvs = {
-		"PWD": path.join(process.env["PWD"], "test/resources/")
-	}
-	
-	var data = {
-		"name": "test-name",
-		"docker_url": "quay.io/mag3llan/test-name"
-	}
-
-	var expectedFileCreatedName = "test-name";
-
-	describe("Run existing file", function() {
-		it("should run", function(done) {
-			var _runnable = {
+	var _runnable = {
 				path: "./test/resources/",
 				fileName: "test.sh",
 				staticEnvs: {
@@ -36,15 +19,31 @@ describe("Runnable", function() {
 				},
 				queries: {
 					"docker_url": "URL",
-					"name": "REPOSITORY_NAME"
+					"name": "REPOSITORY_NAME",
+					"updated_tags:keys():first()": "NEW_TAG",
+					"updated_tags:keys():first():words(?, '.'):first()": "ENVIRONMENT",
+					"array_test:first()": "FIRST_FROM_ARRAY"
 				}
 			}
 
-			var expectedFilePath = path.join(_runnable.path, expectedFileCreatedName)
-			console.log(expectedFilePath);
+	var data = {
+		"name": "test-name",
+		"docker_url": "quay.io/mag3llan/test-name",
+		"updated_tags": {
+			"develop.255": "fc031c925349c6581721ac9f528229a293a0f1f6f9181d8e19f6355209d6f209",
+			"develop.256": "fc031c925349c6581721ac9f528229a293a0f1f6f9181d8e19f6355209d6f209"
+		},
+		"array_test": ["first", "second", "last"]
+	}
+
+	var fileCreatedName = "./test/resources/" + data["name"]; //test-name
+
+	describe("Run existing file", function() {
+		it("should run", function(done) {
+			
 			runnable.run.call(_runnable, data)
 				.then(function() {
-					fs.exists(expectedFilePath, function(exists) {
+					fs.exists(fileCreatedName, function(exists) {
 					    exists.should.be.true;
 					    done();
 					});
@@ -52,6 +51,10 @@ describe("Runnable", function() {
 				.catch(function(err) {
 					done(err);
 				});
+		});
+
+		afterEach(function() {
+			fs.unlink(fileCreatedName);
 		});
 
 	})
